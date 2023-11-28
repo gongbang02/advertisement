8d1a8da1d695ecad70825446072d682bf11a31cb
Bang Gong <gongbang@email.unc.edu>
2023-11-28 18:42:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import cv2
 from gradio.components import Textbox, Image, Video
 from moviepy.editor import VideoFileClip, AudioFileClip
 from controlnet.gradio_scribble import process
-from videocrafter.i2v_test import get_image
+from videocrafter.i2v_test import Image2Video
 from audiocraft.demos.musicgen_app import predict_full
 
 def predict(scribble_prompt, music_prompt, scribble):
     controlNetOut = process(det="Scrible_HED", input_image=scribble, prompt=scribble_prompt, a_prompt="best quality", n_prompt="lowres, bad anatomy, bad hands, cropped, worst quality", num_samples=1, image_resolution=512, detect_resolution=512, ddim_steps=30, guess_mode=False, strength=1.0, scale=9.0, seed=12345, eta=1.0)
-    videoPath = get_image(controlNetOut, scribble_prompt, steps=30, cfg_scale=12.0, eta=1.0, fps=16)
+    videoPath = Image2Video.get_image(controlNetOut, scribble_prompt, steps=30, cfg_scale=12.0, eta=1.0, fps=16)
     video = cv2.VideoCapture(videoPath)
     vidDuration = video.get(cv2.CAP_PROP_POS_MSEC)
     video.release()
