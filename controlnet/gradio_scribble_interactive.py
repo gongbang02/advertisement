58ac50f07d00c9592620e60ecf8025f406995fd7
Bang Gong <gongbang@email.unc.edu>
2023-12-05 18:43:03 -0500

    update

--- controlnet/gradio_scribble_interactive.py
+++ controlnet/gradio_scribble_interactive.py
@@ -69,38 +69,3 @@ def process_interactive(input_image, prompt, a_prompt, n_prompt, num_samples, im
 
 def create_canvas(w, h):
     return np.zeros(shape=(h, w, 3), dtype=np.uint8) + 255
-
-
-block = gr.Blocks().queue()
-with block:
-    with gr.Row():
-        gr.Markdown("## Control Stable Diffusion with Interactive Scribbles")
-    with gr.Row():
-        with gr.Column():
-            canvas_width = gr.Slider(label="Canvas Width", minimum=256, maximum=1024, value=512, step=1)
-            canvas_height = gr.Slider(label="Canvas Height", minimum=256, maximum=1024, value=512, step=1)
-            create_button = gr.Button(label="Start", value='Open drawing canvas!')
-            input_image = gr.Image(source='upload', type='numpy', tool='sketch')
-            gr.Markdown(value='Do not forget to change your brush width to make it thinner. '
-                              'Just click on the small pencil icon in the upper right corner of the above block.')
-            create_button.click(fn=create_canvas, inputs=[canvas_width, canvas_height], outputs=[input_image])
-            prompt = gr.Textbox(label="Prompt")
-            run_button = gr.Button(label="Run")
-            num_samples = gr.Slider(label="Images", minimum=1, maximum=12, value=1, step=1)
-            seed = gr.Slider(label="Seed", minimum=-1, maximum=2147483647, step=1, value=12345)
-            with gr.Accordion("Advanced options", open=False):
-                image_resolution = gr.Slider(label="Image Resolution", minimum=256, maximum=768, value=512, step=64)
-                strength = gr.Slider(label="Control Strength", minimum=0.0, maximum=2.0, value=1.0, step=0.01)
-                guess_mode = gr.Checkbox(label='Guess Mode', value=False)
-                ddim_steps = gr.Slider(label="Steps", minimum=1, maximum=100, value=20, step=1)
-                scale = gr.Slider(label="Guidance Scale", minimum=0.1, maximum=30.0, value=9.0, step=0.1)
-                eta = gr.Slider(label="DDIM ETA", minimum=0.0, maximum=1.0, value=1.0, step=0.01)
-                a_prompt = gr.Textbox(label="Added Prompt", value='best quality')
-                n_prompt = gr.Textbox(label="Negative Prompt", value='lowres, bad anatomy, bad hands, cropped, worst quality')
-        with gr.Column():
-            result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery").style(grid=2, height='auto')
-    ips = [input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, guess_mode, strength, scale, seed, eta]
-    run_button.click(fn=process, inputs=ips, outputs=[result_gallery])
-
-
-block.launch(server_name='0.0.0.0')
